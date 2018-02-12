1646560a9a0ea85eb676423ab77a20d1bcbe7d27
hep <hhhhep@gmail.com>
2018-02-12 20:08:32 +0900

    initial commit

---
+++ src/main/java/com/hep/HepApplication.java
@@ -0,0 +1,12 @@
+package com.hep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HepApplication.class, args);
+	}
+}
