714976c50dda26fae8fb9f9ae4fd46c9b8ed77c2
hep <hhhhep@gmail.com>
2018-03-13 16:41:20 +0900

    본인에 한해 개인정보 수정 가능하도록 구현.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('hep', 'as', '이태원', 'hep@hani.co.kr');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('taewon', 'as', '태원', 'hep@knou.ac.kr');
